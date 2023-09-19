06f34a4465c525f61c7df3618c386e295383322d
amelia azmi <azmiamelia24@gmail.com>
2023-09-19 18:13:13 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          AMELIA AZMI NUR NABAWI
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Learning Docker
+        </p>
       </header>
     </div>
   );
